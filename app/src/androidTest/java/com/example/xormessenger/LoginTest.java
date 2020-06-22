8ab5ef6921b3c9e7699dc2c35c631d163db85526
Gabriel Lee <gabriellee@Gabriels-MacBook-Air.local>
2020-06-22 03:36:30 -0700

    Add UI and Unit tests

--- app/src/androidTest/java/com/example/xormessenger/LoginTest.java
+++ app/src/androidTest/java/com/example/xormessenger/LoginTest.java
@@ -7,8 +7,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.time.Instant;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -17,7 +15,6 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginTest {
--- app/src/androidTest/java/com/example/xormessenger/UsersTest.java
+++ app/src/androidTest/java/com/example/xormessenger/UsersTest.java
@@ -8,11 +8,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class UsersTest {
